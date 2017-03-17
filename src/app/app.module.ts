0e7a27e71bf8f5e109f58a31ba0b5273a02a3e61
fajarachmadyusup13 <fajarachmady13@gmail.com>
2017-03-17 16:15:54 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { AdminModule } from './admin/admin.module';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
+import { BackendApiService } from './backend-api.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AdminModule
   ],
-  providers: [],
+  providers: [
+    BackendApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
